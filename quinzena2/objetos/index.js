e33fe5190c75fbde154f4d7a64f88c6b806ddd70
Lais Oliveira <xlais.oliveira@gmail.com>
2021-07-29 21:25:23 -0300

    Objetos

---
+++ quinzena2/objetos/index.js
@@ -0,0 +1,43 @@
+//Objetos - aula 7
+
+// Exercícios de interpretação de código
+
+/*
+
+1 - Resposta:
+
+Matheus Nachtergaele
+Virginia Cavendish
+{canal: "Globo", horario: "14h"}
+
+2 - Resposta:
+
+{nome: "Juca", idade: 3, raca: "SRD"}
+{nome: "Juba", idade: 3, raca: "SRD"}
+{nome: "Jubo", idade: 3, raca: "SRD"}
+
+
+
+
+3 - Resposta 
+
+false
+undefined
+
+*/
+
+
+//  Exercícios de escrita de código
+
+/*
+
+1 - Resposta:
+
+
+
+
+
+
+
+
+*/
