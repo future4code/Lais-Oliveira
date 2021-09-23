73e458549c317cf3abc2b8cab8e8f13fdc5f66f0
Lais Oliveira <xlais.oliveira@gmail.com>
2021-09-22 21:40:14 -0300

    Recuperação-Arrays

---
+++ recuperacao/recuperacao-arrays/recuperacao-arrays.js
@@ -0,0 +1,34 @@
+//Exercícios de interpretação de código
+
+/*
+
+1 - Resposta:
+
+undefined
+null
+11
+3
+(11) [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+9
+
+
+2 - Resposta:
+
+SUBI NUM ÔNIBUS EM MIRROCOS 
+27
+
+*/
+
+//Exercícios de escrita de código
+
+/*
+1 - Resposta:
+
+const emailDoUsuario = prompt("Qual seu email?")
+const nomeDoUsuario = prompt("Qual seu nome?")
+
+console.log("O email " + emailDoUsuario +" foi cadastrado com sucesso. Seja bem-vinda(o) " + nomeDoUsuario)
+
+2 - Resposta:
+3 - Resposta:
+*/
